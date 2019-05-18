525c0c0e3952d201d627d62a395da30475149b67
Charles Buhler <charlesbuhler@gmail.com>
2019-05-18 16:52:38 -0700

    Stringing together Services and Transformers.

--- backend/Services/RetrofitService.js
+++ backend/Services/RetrofitService.js
@@ -4,52 +4,21 @@
  * Service to call the ML model and determine the usage percentile.
  */
 
-// Front end will send the following schema:
-
-/*
-AIR: false
-BEDROOMS: "5"
-CITY: "San Francisco"
-DECADE_BUILT: "1970"
-GARAGE: false
-HEAT: true
-SOLAR_Boolean: true
-TOTAL_AREA: "1445"
-*/
-
-// Needs to be serialized into backend model here:
-
- // H heat B both A air-conditioning, N neither
- //
-// {
-// "CITY": "San Francisco",
-// "SOLAR_Boolean": 1,
-// "BEDROOMS": 5,
-// "GARAGE": 1,
-// "HEAT_AIR_COND" 'B',
-// "TOTAL_AREA": 1234,
-// "TOTAL_ROOMS" 5,
-// "DECADE_BUILT": "1970",
-// }
-
 const { transform } = require('../Transformers/UsageProfileToMLModelTransformer');
 
 const validateSchema = usageProfile => {
-  // const keys = usageProfile.keys();
-  // console.log('k', keys)
-  // if (!keys.contains('AIR')) {
-  //   return false;
-  // }
+  const keys = Object.keys(usageProfile);
+  if (!keys.includes('CITY')) {
+    return false;
+  }
   // TODO: Add rest of schema
   return true;
 }
-
 
 const getUsagePercentile = usageProfile => {
   if (!validateSchema(usageProfile)) {
     return "Missing data."
   }
-
 
   const MLModelData = transform(usageProfile);
 
